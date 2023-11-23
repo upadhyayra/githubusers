ed81fb8958aa78f5fdc408ca2dbb271d78e02548
upadhyayra <rahulupadhyay1324@gmail.com>
2023-11-23 15:39:56 +0530

    jabcvjkab

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Github from "./Github";
 import useFetch from "./useFetch";
+import Loading from "./assets/loading.gif"
 
 const Home = () => {
 
@@ -8,13 +9,30 @@ const Home = () => {
 
   return (
     <>
-      <h2 className="bg-secondary mt-2 p-2 w-50 m-auto rounded">List of GitHub Users</h2>
+      <h2 className="bg-secondary mt-2 p-2 w-50 m-auto rounded">
+        List of GitHub Users
+      </h2>
       <div className="container mt-5 ">
         <div className="row">
-          {
-              
-          data && data.map((items) => <Github items={items} />
-            )}
+          {data == "" ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "500px",
+              }}
+            >
+              <img
+                src={Loading}
+                alt=""
+                style={{ width: "10%", height: "20%" }}
+              />
+            </div>
+          ) : (
+            
+            data && data.map((items) => <Github items={items} />)
+          )}
         </div>
       </div>
     </>
